7288b86a6f03d4c38d53fccca190da243c30aab6
FedeRuiz <federicomaruiz@gmail.com>
2022-04-05 18:38:25 +0200

    login-completo

---
+++ token.js
@@ -0,0 +1,34 @@
+export function isTokenExpired() {
+    const token = localStorage.getItem("token")
+
+    if (!token)
+        return true
+
+    try {
+        const expiry = (JSON.parse(atob(token.split('.')[1]))).exp;
+        const now = (Math.floor((new Date).getTime() / 1000))
+
+        return now >= expiry;
+    } catch (error) {
+        return true
+    }
+
+}
+
+
+function isTokenValid() {
+    const token = localStorage.getItem("token")
+    console.log(token)
+    if (!token)
+        return false
+
+    try {
+        const expiry = (JSON.parse(atob(token.split('.')[1]))).exp;
+        const now = (Math.floor((new Date).getTime() / 1000))
+
+        return now < expiry;
+    } catch (error) {
+        return false
+    }
+
+}
